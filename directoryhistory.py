bbeaf1891c307a9ffc4c29c28e7d026b3a9314df
Sharky_001 <71152134+KrechkoVsevolod201@users.noreply.github.com>
2021-10-26 22:49:08 +0400

    test class

--- directoryhistory.py
+++ directoryhistory.py
@@ -1,7 +1,17 @@
-# fileName = ""
+from datetime import datetime
 
 def openAndAppend():
-    f = open('D:\PycharmProjects\ProjectCut\AsistFiles\Help.txt', 'r')
+    current_datetime = datetime.now()
+    year = current_datetime.year
+    month = current_datetime.month
+    day = current_datetime.day
+    hour = current_datetime.hour
+    minute = current_datetime.minute
+    second = current_datetime.second
+
+    datetimestr = (year + month + day + hour + minute + second)
+    print(datetimestr)
+    f = open('D:\PycharmProjects\ProjectCut\AsistFiles\FileWay.txt', 'r')
     fileName = f.read()
     f.close()
     print(fileName)
